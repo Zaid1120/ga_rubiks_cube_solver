750d5ab0dc880f943d59866e4f38d83b1c69bcdb
Zaid1120 <116662451+Zaid1120@users.noreply.github.com>
2023-11-01 21:08:15 +0000

    adjustments

--- src/rubiks.py
+++ src/rubiks.py
@@ -1,54 +1,30 @@
 import copy
 
 cube_dev = {  # this cube is for testing
-    "U": [["U1", "U2", "U3"],
-          ["U4", "U5", "U6"],
-          ["U7", "U8", "U9"]],
-    "R": [["R1", "R2", "R3"],
-          ["R4", "R5", "R6"],
-          ["R7", "R8", "R9"]],
-    "L": [["L1", "L2", "L3"],
-          ["L4", "L5", "L6"],
-          ["L7", "L8", "L9"]],
-    "D": [["D1", "D2", "D3"],
-          ["D4", "D5", "D6"],
-          ["D7", "D8", "D9"]],
-    "F": [["F1", "F2", "F3"],
-          ["F4", "F5", "F6"],
-          ["F7", "F8", "F9"]],
-    "B": [["B1", "B2", "B3"],
-          ["B4", "B5", "B6"],
-          ["B7", "B8", "B9"]],
+    "U": [["U1", "U2", "U3"], ["U4", "U5", "U6"], ["U7", "U8", "U9"]],
+    "R": [["R1", "R2", "R3"], ["R4", "R5", "R6"], ["R7", "R8", "R9"]],
+    "L": [["L1", "L2", "L3"], ["L4", "L5", "L6"], ["L7", "L8", "L9"]],
+    "D": [["D1", "D2", "D3"], ["D4", "D5", "D6"], ["D7", "D8", "D9"]],
+    "F": [["F1", "F2", "F3"], ["F4", "F5", "F6"], ["F7", "F8", "F9"]],
+    "B": [["B1", "B2", "B3"], ["B4", "B5", "B6"], ["B7", "B8", "B9"]],
 }
 
 cube_main = {
-    "U": [["U", "U", "U"],
-          ["U", "U", "U"],
-          ["U", "U", "U"]],
-    "R": [["R", "R", "R"],
-          ["R", "R", "R"],
-          ["R", "R", "R"]],
-    "L": [["L", "L", "L"],
-          ["L", "L", "L"],
-          ["L", "L", "L"]],
-    "D": [["D", "D", "D"],
-          ["D", "D", "D"],
-          ["D", "D", "D"]],
-    "F": [["F", "F", "F"],
-          ["F", "F", "F"],
-          ["F", "F", "F"]],
-    "B": [["B", "B", "B"],
-          ["B", "B", "B"],
-          ["B", "B", "B"]],
+    "U": [["U", "U", "U"], ["U", "U", "U"], ["U", "U", "U"]],
+    "R": [["R", "R", "R"], ["R", "R", "R"], ["R", "R", "R"]],
+    "L": [["L", "L", "L"], ["L", "L", "L"], ["L", "L", "L"]],
+    "D": [["D", "D", "D"], ["D", "D", "D"], ["D", "D", "D"]],
+    "F": [["F", "F", "F"], ["F", "F", "F"], ["F", "F", "F"]],
+    "B": [["B", "B", "B"], ["B", "B", "B"], ["B", "B", "B"]],
 }
 
 
 def rotate_face(cube,  face, clockwise=True):
     if clockwise:
-        # Clockwise rotation: last column -> first row, middle column -> middle row, first column -> last row
+        # clockwise rotation: last column -> first row, middle column -> middle row, first column -> last row
         return [list(reversed(i)) for i in zip(*cube[face])]
     else:
-        # Counter-clockwise rotation: first column -> first row, middle column -> middle row, last column -> last row
+        # counter-clockwise rotation: first column -> first row, middle column -> middle row, last column -> last row
         return [list(i) for i in reversed(list(zip(*cube[face])))]
 
 
